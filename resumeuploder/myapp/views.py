d757021bdf420f64a287d3e5d5b1123be19a9e1f
kishoreMS07 <130275065+kishoreMS07@users.noreply.github.com>
2023-10-15 20:43:55 +0530

    Add files via upload

---
+++ resumeuploder/myapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .forms import ResumeForm
+from .models import Resume
+from django.views import View
+class HomeView(View):
+    def get(self, request):
+        form = ResumeForm()
+        candidates = Resume.objects.all()
+        return render(request, 'myapp/home.html' , {'candidates': candidates , 'form':form})
+     
+    def post(self, request):
+        form = ResumeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'myapp/home.html' , {'form':form})
+class CandidateView(View):
+    def get(self, request, pk):
+        candidate = Resume.objects.get(pk=pk)
+        return render(request, 'myapp/candidate.html', {'candidate':candidate})
+    from django.shortcuts import render
+
+def your_view(request):
+    # Your view logic here
+    return render(request, 'myapp/home.html', {'candidates': candidates, 'form': form})
